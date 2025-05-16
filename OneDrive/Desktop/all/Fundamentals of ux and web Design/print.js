c65d60ad6e2ce35b25876c54f55fcb21fed4d8cb
abhimanyu-kotari <abhimanyukotari4@gmail.com>
2025-05-16 15:36:01 +0530

    Add frontend project files

---
+++ OneDrive/Desktop/all/Fundamentals of ux and web Design/print.js
@@ -0,0 +1,6 @@
+let a = 5; // Example value for a
+let b = 3; // Example value for b
+
+let sum = a + b;
+
+console.log("The sum of a and b is: " + sum);
---
+++ OneDrive/Desktop/all/Fundamentals of ux and web Design/sunmonJAVA.js
@@ -0,0 +1,4 @@
+var num=7; switch(num) { case1: console.log("sunday"); break; case2:
+console.log("monady"); break; case3: console.log("tuesday"); break; case4:
+console.log("wednesday"); break; case5: console.log("thursday"); break; case6:
+console.log("friday"); break; case7: console.log("saturday"); break; }
